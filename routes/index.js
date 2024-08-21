8eff495415254768490eae71eab17b8fc2e4ab0f
Martin Chorley <martin.chorley@gmail.com>
2024-08-21 18:58:52 +0100

    more tidying

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ var router = express.Router();
 
 const strava = require('strava-v3');
 
-
 /* GET home page. */
 router.get('/', async function(req, res, next) { 
-  console.log(req.user);
   res.render('index', { title: 'Strava Maps', user: req.user });
 });
 
